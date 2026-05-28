737fdd48a5176dbfe23e6ddcc9f4da4a21284be0
Skye S <noob1957@outlook.com>
2026-05-27 18:15:19 -0700

    continued work on this, implemented second turtle on screen to display the scan output

--- working_files/Controls/Virtual maze debugger/Virtual maze.py
+++ working_files/Controls/Virtual maze debugger/Virtual maze.py
@@ -17,8 +17,8 @@ cell_pos = [a/2, a/2]       #the (current) position of the car in the cell itsel
 cell_pos_temp = cell_pos    #the temporary value that can be changed and used during the search algorithm
 
 
-#walls is a n*m*2 array of boolean/ 1 or 2, which is a n*m grid of an array of 2, so n*m is the position, and the first bool of that position is if the bottom wall exist, and the second is if the left wall exist
-walls = []                  #creates the n*m grid of cell walls
+#walls is a n*m*2 array of boolean/ 0 or 1, which is a n*m grid of an array of 2, so n*m is the position, and the first bool of that position is if the bottom wall exist, and the second is if the left wall exist
+walls = []
 for i in range (n+1):         #THIS WILL PROBABLY BE REPLACED BY A SEPERATE PROGRAM THAT CREATES THE 2D ARRAY *WITH* THE WALLS (currently it's just empty)
     temp=[]
     for j in range (m+1):
@@ -45,8 +45,17 @@ def scan_sweep():
     
         len_sum = 0             #reset this section's total length (for average) and minimum length
         len_min = 3000
-        for offset in range(10):    #sweeps through the 10 degrees of this segment, could be made finer or coarser
-            len_curr = scan_search(angle * 10 + offset, 0)
+        for offset in range(-5, 5, 1):    #sweeps through the 10 degrees of this segment, could be made finer or coarser
+            global curr_pos
+            global curr_pos_temp 
+            curr_pos_temp = curr_pos
+
+            global cell_pos
+            global cell_pos_temp 
+            cell_pos_temp = cell_pos
+
+            len_curr = scan_search((angle * 10 + offset + 360) % 360, 0)
+            print(len_curr)
             len_sum += len_curr     #adds total length and find's minimum length
             len_min = min(len_min, len_curr)
         
@@ -55,24 +64,30 @@ def scan_sweep():
 
 def scan_search(angle, length):
 
+    global curr_pos_temp
+    global cell_pos_temp
+
     if (length>=2560):          #exit case, emulating if the scan goes beyond the lidar's maximum scan distance
         return 2560.0
     
+    angle = angle * math.pi / 180
+
     cell_pos_temp[0] += math.sin(angle)     #adds to the distance by 1 in the angle theta
     cell_pos_temp[1] += math.cos(angle)
+    print(cell_pos_temp)
 
     collision = False                    #booleans for if collision happened
 
 
     if cell_pos_temp[0] > a:    #checks if it moves beyond right side of cell
-        if walls[curr_pos_temp[0] + 1, curr_pos_temp[1], 0] == 1:  #if the right side of the cell exists a wall
+        if walls[curr_pos_temp[0] + 1][curr_pos_temp[1]][1] == 1:  #if the right side of the cell exists a wall
             collision = True
         else:
             curr_pos_temp[0] += 1
             cell_pos_temp[0] -= a
 
     elif cell_pos_temp[0] < 0:  #checks if it moves beyond left side of cell
-        if walls[curr_pos_temp[0], curr_pos_temp[1], 0] == 1:  #if the left side of the cell exists a wall
+        if walls[curr_pos_temp[0]][curr_pos_temp[1]][1] == 1:  #if the left side of the cell exists a wall
             collision = True
         else:
             curr_pos_temp[0] -= 1
@@ -80,15 +95,14 @@ def scan_search(angle, length):
 
         
     if cell_pos_temp[1] > a:    #checks if it moves beyond up side of cell
-        print("a", curr_pos_temp[0], curr_pos_temp[1] + 1, 1)
-        if walls[curr_pos_temp[0], curr_pos_temp[1] + 1, 1] == 1:  #if the up side of the cell exists a wall
+        if walls[curr_pos_temp[0]][curr_pos_temp[1] + 1][0] == 1:  #if the up side of the cell exists a wall
             collision = True
         else:
             curr_pos_temp[1] += 1
             cell_pos_temp[1] -= a
 
     elif cell_pos_temp[1] < 0:  #checks if it moves beyond down side of cell
-        if walls[curr_pos_temp[0], curr_pos_temp[1], 1] == 1:  #if the down side of the cell exists a wall
+        if walls[curr_pos_temp[0]][curr_pos_temp[1]][0] == 1:  #if the down side of the cell exists a wall
             collision = True
         else:
             curr_pos_temp[1] -= 1
@@ -103,14 +117,16 @@ def scan_search(angle, length):
 
 def scan_draw():
     for section in scan_output:
+        print()
+        print(section)
+        print(section[0])
         turt_scan.goto(a * (n+1) / 2 , 0)
-        turt_scan.degrees(section[0])
+        turt_scan.setheading(section[0])
         turt_scan.pendown()
-        turt_scan.pensize(3)
         turt_scan.forward(section[1])
-        turt_scan.pensize(1)
+        turt_scan.penup()
         turt_scan.forward(section[2]-section[1])
-        turt_scan.pendown()
+        turt_scan.dot()
 
 #scan_sweep()
 
@@ -118,7 +134,7 @@ print(walls)
 print(scan_output)
 
         
-turt_scan.speed(0)
+#turt_scan.speed(0)
 turt_scan.penup()
 turt_maze.speed(0) #we can probably slow down the turtle move speed to slow down the program running speed
 
@@ -142,7 +158,7 @@ for i in range(n+1):
             turt_maze.penup()
 
 
-
+turt_maze.goto(a * (-(n+1) / 2 + curr_pos[0]) + cell_pos[0], a * (-(m+1) / 2 + curr_pos[1]) + cell_pos[0])
 
 
 input()
